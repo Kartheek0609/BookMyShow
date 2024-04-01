348bdd80dcc7c834487f7c126059c25acaa0e76d
Kartheek0609 <surasanikartheekreddy@gmail.com>
2024-04-01 16:58:08 +0530

    pom.xml file changed

--- src/main/java/dev/kartheek/BookMyShow/model/Show.java
+++ src/main/java/dev/kartheek/BookMyShow/model/Show.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Getter
 @Setter
-@Entity
+@Entity(name="BMS_Show")
 public class Show extends BaseModel{
     private LocalDateTime startTime;
     private LocalDateTime endTime;
